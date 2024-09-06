d780a62589dde1039ee663569d22e62e2a470f28
Thanga Prakash <Prakash@Karthicks-MacBook-Air.local>
2024-09-06 10:08:28 +0530

    Styling Commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import upcoming_movies from '../images/upcoming_movies.png'
 import goat from '../images/goat.png'
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import movies from '../apiData/movies.json'
 
 export const Home = () => {
   
@@ -24,53 +25,57 @@ export const Home = () => {
     </div>
       <div>
       
-        <div className='flex-items-center justify-center-screen'>
-        <h1 className='text-center '> Movies Image Slider</h1>
-        <img src = {goat} className='cursor-pointer' />
-        </div>
+        
+
+<h1 className='text-center '> Movies Image Slider</h1>
+        <div className="flex justify-center items-center h-full">
+      <img src={goat} alt="Image" className="w-100 h-40 object-cover" />
+    </div>
       
       </div>
           
-          <hr className='border border-black mt-38'/>
+          <hr className='border border-black mt-12'/>
           <h1 className='text-center mt-2'>Recommended Movies</h1>
 
-          <div className="flex justify-between px-8">
-      {/* Wrapper for all images and buttons */}
+
+
+     <div className="flex justify-center px-8">
+     
       <div className="flex space-x-4">
-        {/* Image and Button 1 */}
+       
+     
         <div className="text-center">
-          <img src={goat} alt="Image 1" className="w-32 h-32 object-cover" />
+          <img src={goat}  alt="Image 1" className="w-40 h-40 object-cover" />
           <button className="mt-2 bg-blue-500 text-white py-2 px-4 rounded">
             Book
           </button>
         </div>
 
-        {/* Image and Button 2 */}
+      
         <div className="text-center">
-          <img src={goat} alt="Image 2" className="w-32 h-32 object-cover" />
+          <img src={goat}  alt="Image 2" className="w-40 h-40 object-cover" />
           <button className="mt-2 bg-blue-500 text-white py-2 px-4 rounded">
             Book
           </button>
         </div>
 
-        {/* Image and Button 3 */}
+       
         <div className="text-center">
-          <img src={goat} alt="Image 3" className="w-32 h-32 object-cover" />
+          <img src={goat}  alt="Image 3" className="w-40 h-40 object-cover" />
           <button className="mt-2 bg-blue-500 text-white py-2 px-4 rounded">
             Book
           </button>
         </div>
 
-        {/* Image and Button 4 */}
+     
         <div className="text-center">
-          <img src={goat} alt="Image 4" className="w-32 h-32 object-cover" />
+          <img src={goat} alt="Image 4" className="w-40 h-40 object-cover" />
           <button className="mt-2 bg-blue-500 text-white py-2 px-4 rounded">
             Book
           </button>
         </div>
       </div>
     </div>
-
 
 
 
